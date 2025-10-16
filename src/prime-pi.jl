944bba2a99c3ffef874ab338bb992b7498bffd63
obluda2173 <obluda2173@gmail.com>
2025-10-17 01:22:47 +0200

    storing primes

--- src/prime-pi.jl
+++ src/prime-pi.jl
@@ -32,7 +32,23 @@ end
 # second()
 
 
-function third(filename, rounds=20, max_length=1000)
+function write_to_file(filename, prime)
+    fd = open(filename, "w")
+    write(fd, prime)
+    close(fd)
+end
+
+function is_divisible_by_small_primes(num)
+    small_primes = [3, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    for prime in small_primes
+        if num % prime == 0 && num != prime
+            return true
+        end
+    end
+    return false
+end
+
+function third(filename, rounds=20, max_length=6205)
     str = read(filename, String)
     digits_str = str[3:end] # skip the "1."
 
@@ -45,11 +61,17 @@ function third(filename, rounds=20, max_length=1000)
 
         num = parse(BigInt, substring)
 
+        if is_divisible_by_small_primes(num)
+            continue
+        end
+
         if isprime(num, rounds)
+            prime_filename = "./primes/prime-$(len).txt"
             accuracy = 1 - 4^float(-rounds)
-            println("Found prime prefix of length $len")
-            println("Accuracy of the found number being prime is: ", accuracy)
-            println("Prime number: $(substring[1:len])")
+            println("Found prime has length of: $len")
+            println("Accuracy of the found number being prime is: $accuracy")
+            write_to_file(prime_filename, substring[1:len])
+            println("Prime number has been stored into $prime_filename")
             return num
         end
 
@@ -59,4 +81,4 @@ function third(filename, rounds=20, max_length=1000)
     return nothing
 end
 
-third("pi.txt")
+third("pi.txt", 2)
